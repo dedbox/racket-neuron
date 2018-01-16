40b0943adbf6fdf51fb744d095beb2f7a0c663e0
Eric Griffis <dedbox@gmail.com>
2018-01-15 17:06:35 -0800

    implement IPC and unit tests

--- process.rkt
+++ process.rkt
@@ -233,3 +233,217 @@
   (test-case
     "wait raises unhandled on unhandled exceptions."
     (check-exn unhandled? (λ () (wait (start (λ () (raise #t))))))))
+
+;;; Inter-Process Communication
+
+(provide
+ (contract-out
+  [msg/c flat-contract?]
+  [give (->* (process?) (msg/c) boolean?)]
+  [take (-> any/c)]
+  [try-take (-> any/c)]
+  [emit (->* () (msg/c) void?)]
+  [recv (-> process? any/c)]
+  [try-recv (-> process? any/c)]
+  [give-evt (->* (process?) (msg/c) evt?)]
+  [take-evt (-> evt?)]
+  [emit-evt (->* () (msg/c) evt?)]
+  [recv-evt (-> process? evt?)]))
+
+(define msg/c (not/c eof-object?))
+
+;; Commands
+
+(define (give π [v (void)])
+  (sync (give-evt π v)))
+
+(define (take)
+  (sync (take-evt)))
+
+(define (try-take)
+  (channel-try-get (process-input-ch (current-process))))
+
+(define (emit [v (void)])
+  (sync (emit-evt v)))
+
+(define (recv π)
+  (sync (recv-evt π)))
+
+(define (try-recv π)
+  (channel-try-get (process-output-ch π)))
+
+;; Events
+
+(define (give-evt π [v (void)])
+  (choice-evt
+    (handle-evt (channel-put-evt (process-input-ch π) v) (λ _ #t))
+    (handle-evt π (λ _ #f))))
+
+(define (take-evt)
+  (process-input-ch (current-process)))
+
+(define (emit-evt [v (void)])
+  (handle-evt (channel-put-evt (process-output-ch (current-process)) v) void))
+
+(define (recv-evt π)
+  (choice-evt
+    (process-output-ch π)
+    (handle-evt π (λ _ eof))))
+
+(module+ test
+  (require rackunit)
+
+  (test-case
+    "msg/c accepts any non-eof value."
+    (check-true (msg/c 0))
+    (check-true (msg/c 'x))
+    (check-true (msg/c (start deadlock))))
+
+  (test-case
+    "msg/c rejects eof."
+    (check-false (msg/c eof)))
+
+  ;; Events
+
+  (test-case
+    "A give-evt is ready when π accepts v."
+    (define π (start (λ () (emit) (take))))
+    (define evt (give-evt π))
+    (check-false (sync/timeout 0 evt))
+    (recv π)
+    (check-false (not (sync evt))))
+
+  (test-case
+    "A give-evt syncs to #t if π accepts v."
+    (check-true (sync (give-evt (start (λ () (take)))))))
+
+  (test-case
+    "A give-evt syncs to #f if π dies before accepting v."
+    (define π (start deadlock))
+    (define evt (give-evt π))
+    (kill π)
+    (check-false (sync evt)))
+
+  (test-case
+    "A take-evt is ready when a process provides a value."
+    (define π (start (λ () (check-false (not (sync (take-evt)))))))
+    (give π)
+    (wait π))
+
+  (test-case
+    "A take-evt syncs to the provided value."
+    (define π (start (λ () (check eq? (sync (take-evt)) 3))))
+    (give π 3)
+    (wait π))
+
+  (test-case
+    "An emit-evt is ready when a process accepts v."
+    (define π (start (λ () (check-false (not (sync (emit-evt)))))))
+    (recv π)
+    (wait π))
+
+  (test-case
+    "An emit-evt syncs to void."
+    (define π (start (λ () (check-pred void? (sync (emit-evt))))))
+    (recv π)
+    (wait π))
+
+  (test-case
+    "A recv-evt is ready when a value is accepted from π."
+    (define π (start (λ () (take) (emit))))
+    (define evt (recv-evt π))
+    (check-false (sync/timeout 0 evt))
+    (give π)
+    (check-false (not (sync evt))))
+
+  (test-case
+    "A recv-evt syncs to the value accepted from π."
+    (check = (sync (recv-evt (start (λ () (emit 5))))) 5))
+
+  (test-case
+    "A recv-evt syncs to eof if π dies."
+    (define π (start deadlock))
+    (define evt (recv-evt π))
+    (kill π)
+    (check-pred eof-object? (sync evt)))
+
+  ;; Commands
+
+  (test-case
+    "give blocks until π accepts v."
+    (check-false (not (give (start (λ () (take)))))))
+
+  (test-case
+    "give blocks until π dies."
+    (check-false (give (start die))))
+
+  (test-case
+    "give returns #t if π accepts v."
+    (check-true (give (start (λ () (take))))))
+
+  (test-case
+    "give returns #f if π dies before accepting v."
+    (check-false (give (start die))))
+
+  (test-case
+    "take blocks until a value is provided to π."
+    (define π (start (λ () (check-false (not (take))))))
+    (give π)
+    (wait π))
+
+  (test-case
+    "take returns the provided value."
+    (define π (start (λ () (check = (take) 7))))
+    (give π 7)
+    (wait π))
+
+  (test-case
+    "try-take returns the provided value if any."
+    (define π1 (start (λ () (emit) (check = (try-take) 11))))
+    (define π2 (start (λ () (give π1 11))))
+    (sync/timeout 0.1 π2)
+    (recv π1)
+    (wait π2)
+    (wait π1))
+
+  (test-case
+    "try-take returns #f if no value is provided."
+    (wait (start (λ () (check-false (try-take))))))
+
+  (test-case
+    "emit blocks until a process accepts v."
+    (define π (start (λ () (check-false (not (emit))))))
+    (recv π)
+    (wait π))
+
+  (test-case
+    "emit returns void."
+    (define π (start (λ () (check-pred void? (emit)))))
+    (recv π)
+    (wait π))
+
+  (test-case
+    "recv blocks until a value is accepted from π."
+    (check-false (not (recv (start (λ () (emit)))))))
+
+  (test-case
+    "recv blocks until π dies."
+    (check-false (not (recv (start die) ))))
+
+  (test-case
+    "recv returns the value accepted from π."
+    (check = (recv (start (λ () (emit 13)))) 13))
+
+  (test-case
+    "recv returns eof when π dies."
+    (check-pred eof-object? (recv  (start die))))
+
+  (test-case
+    "try-recv returns the value accepted from π if any."
+    (define π (start (λ () (emit 17))))
+    (sync/timeout 0.1 π)
+    (check = (try-recv π) 17))
+
+  (test-case
+    "try-recv returns #f if no value is accepted."
+    (check-false (try-recv (start deadlock) ))))
