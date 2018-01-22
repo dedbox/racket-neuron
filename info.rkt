0b2d7e242d5d47415cdf39ff2c8de4b2c31b9d82
Eric Griffis <dedbox@gmail.com>
2018-01-21 19:04:17 -0800

    add sandbox-lib dependency

--- info.rkt
+++ info.rkt
@@ -5,7 +5,12 @@
 (define deps '("base"))
 
 (define build-deps
-  '("at-exp-lib" "pict-lib" "racket-doc" "rackunit-lib" "scribble-lib"))
+  '("at-exp-lib"
+    "pict-lib"
+    "racket-doc"
+    "rackunit-lib"
+    "sandbox-lib"
+    "scribble-lib"))
 
 (define scribblings
   '(("scribblings/main.scrbl" (multi-page) (library) "neuron")))
