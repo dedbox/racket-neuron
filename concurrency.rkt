ba3acf9c53da6598166eac8f7f1ff5ad1e8ebe1d
Eric Griffis <dedbox@gmail.com>
2018-01-26 10:34:50 -0800

    simplified concurrency exports

--- concurrency.rkt
+++ concurrency.rkt
@@ -1,12 +1,7 @@
 #lang racket/base
 
-(require neuron/private/require
-         racket/contract/base)
+(require neuron/private/require)
 
-(require neuron/concurrency/process)
-(provide
- (except-out (all-from-out neuron/concurrency/process) (struct-out process))
- (contract-out [process? predicate/c]))
-
-(require-and-provide neuron/concurrency/ipc
+(require-and-provide neuron/concurrency/process
+                     neuron/concurrency/ipc
                      neuron/concurrency/control)
