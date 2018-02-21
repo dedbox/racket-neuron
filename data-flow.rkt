09bf355f631c8f746ce3566e32b892998c387263
Eric Griffis <dedbox@gmail.com>
2018-02-21 06:17:27 -0800

    rename data-flow/ports to data-flow/socket

--- data-flow.rkt
+++ data-flow.rkt
@@ -2,6 +2,6 @@
 
 (require neuron/private/require)
 
-(require-and-provide neuron/data-flow/ports
+(require-and-provide neuron/data-flow/socket
                      neuron/data-flow/codecs
                      neuron/data-flow/networking)
