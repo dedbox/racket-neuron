3d1322f8886bb91d1c9e128f8d7d2c5282b69d3f
Eric Griffis <dedbox@gmail.com>
2018-03-05 20:29:27 -0800

    add concurrency to main exports

--- main.rkt
+++ main.rkt
@@ -2,5 +2,7 @@
 
 (require neuron/private/require)
 
-(require-and-provide neuron/concurrency
-                     neuron/data-flow)
+(require-and-provide
+ neuron/evaluation
+ neuron/concurrency
+ neuron/data-flow)
